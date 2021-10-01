3011b585239baed1d9c23f8b7d5a375514f51a85
Uncle Scientist <unclescientist@gmail.com>
2021-10-01 08:55:52 -0400

    fizzy buzz code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    for j in 1..=15 {
+        for i in 0..8 {
+            let output =    if j % 15 == 0 {
+                                format!("FizzBuzz")
+                            } else if j % 5 == 0 {
+                                format!("Buzz")
+                            } else if j % 3 == 0 {
+                                format!("Fizz")
+                            } else {
+                                format!("{}", i * 15 + j)
+                            };
+
+            print!("{:<10}", output);
+        }
+        println!("");
+    }
 }
