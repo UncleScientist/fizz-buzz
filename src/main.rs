2c5146e70560763652eb6079724d39b0385be846
Uncle Scientist <unclescientist@gmail.com>
2021-10-01 08:57:26 -0400

    fmt and make clippy happy

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 fn main() {
     for j in 1..=15 {
         for i in 0..8 {
-            let output =    if j % 15 == 0 {
-                                format!("FizzBuzz")
-                            } else if j % 5 == 0 {
-                                format!("Buzz")
-                            } else if j % 3 == 0 {
-                                format!("Fizz")
-                            } else {
-                                format!("{}", i * 15 + j)
-                            };
+            let output = if j % 15 == 0 {
+                "FizzBuzz".to_string()
+            } else if j % 5 == 0 {
+                "Buzz".to_string()
+            } else if j % 3 == 0 {
+                "Fizz".to_string()
+            } else {
+                format!("{}", i * 15 + j)
+            };
 
             print!("{:<10}", output);
         }
-        println!("");
+        println!();
     }
 }
